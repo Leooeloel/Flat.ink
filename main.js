3482c539a94a7c30b14309a152c53e3de39c982d
Leooeloel <1826234293@qq.com>
2022-05-04 15:07:28 +0800

    fix: fix location link error

--- main.js
+++ main.js
@@ -1,46 +1,45 @@
-void function() {
-    "use strict"
+void (function () {
+  "use strict";
 
-    const $ = sel => document.querySelector(sel)
-    const $$ = sel => Array.from(document.querySelectorAll(sel))
-    Node.prototype.on = Node.prototype.addEventListener
+  const $ = (sel) => document.querySelector(sel);
+  const $$ = (sel) => Array.from(document.querySelectorAll(sel));
+  Node.prototype.on = Node.prototype.addEventListener;
 
-    let tabs = $$('.tabs .items .item')
-    let imgs = $$('.tabs .images img')
-    function updateTab(activeTab) {
-        tabs.forEach((tab, i) => {
-            const visible = tab.classList.toggle('active', tab === activeTab)
-            imgs[i].style.display = visible ? '' : 'none'
-        });
-    }
-    tabs.forEach(tab => tab.on('click', updateTab.bind(null, tab)))
+  let tabs = $$(".tabs .items .item");
+  let imgs = $$(".tabs .images img");
+  function updateTab(activeTab) {
+    tabs.forEach((tab, i) => {
+      const visible = tab.classList.toggle("active", tab === activeTab);
+      imgs[i].style.display = visible ? "" : "none";
+    });
+  }
+  tabs.forEach((tab) => tab.on("click", updateTab.bind(null, tab)));
 
+  const langKey = "flat:language";
+  const languages = [
+    { key: "zh-CN", name: "简体中文" },
+    { key: "en", name: "English" },
+  ];
+  const langHref = {
+    "zh-CN": "/",
+    en: "/en/",
+  };
+  const currentLanguage =
+    location.pathname.split("/").filter(Boolean)[0] || "zh-CN";
 
-    const langKey = "flat:language"
-    const languages = [
-        { key: "zh-CN", name: "简体中文" },
-        { key: "en", name: "English" },
-    ]
-    const langHref = {
-        "zh-CN": "/",
-        "en": "/en/",
-    }
-    const currentLanguage = location.pathname.split("/").filter(Boolean)[0] || "zh-CN"
+  let select = $("#lang");
+  select.innerHTML = "";
+  languages.forEach(({ key, name }) => {
+    const option = document.createElement("option");
+    option.value = key;
+    option.textContent = name;
+    select.append(option);
+  });
+  select.value = currentLanguage;
 
-    let select = $('#lang')
-    select.innerHTML = ""
-    languages.forEach(({ key, name }) => {
-        const option = document.createElement('option')
-        option.value = key
-        option.textContent = name
-        select.append(option)
-    })
-    select.value = currentLanguage
-
-    select.on('change', (e) => {
-        const language = e.target.value
-        localStorage.setItem(langKey, language)
-        window.location.href = langHref[language]
-    })
-
-}();
+  select.on("change", (e) => {
+    const language = e.target.value;
+    localStorage.setItem(langKey, language);
+    window.location.href += langHref[language];
+  });
+})();
